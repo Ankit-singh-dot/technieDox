79d8e58c14cd07f6b4acdf122ac35e4c76c1b963
Ankit <ankitrajdihara123@gmail.com>
2026-05-03 23:10:59 +0530

    index_page

---
+++ app/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- <Navbar /> -->
+  <Hero />
+  <FeatureSection />
+  <AnalyticsSection />
+  <PricingPreview />
+  <UseCasesSection />
+  <CommunitySection />
+  <CTAFloating/>
+  <FooterSection />
+</template>
+
+<script setup>
+// import Navbar from "../../components/Navbar.vue";
+import AnalyticsSection from "~/components/sections/AnalyticsSection.vue";
+import Hero from "../components/sections/Hero.vue";
+import FeatureSection from "~/components/sections/FeatureSection.vue";
+import PricingPreview from "~/components/sections/PricingPreview.vue";
+import UseCasesSection from "~/components/sections/UseCasesSection.vue";
+import CommunitySection from "~/components/sections/CommunitySection.vue";
+import FooterSection from "~/components/footer/FooterSection.vue";
+import CTAFloating from '~/components/sections/Footer/CTAFloating.vue'
+
+</script>
