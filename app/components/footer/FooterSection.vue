aa90e80b68f68831c6ca3412ecd3db70e9fab57d
Ankit <ankitrajdihara123@gmail.com>
2026-05-03 23:59:40 +0530

    footer_added

---
+++ app/components/footer/FooterSection.vue
@@ -0,0 +1,72 @@
+
+<template>
+  <footer class="bg-[#f5f4f4] pt-72 pb-20 ">
+
+    <div class="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-start gap-16">
+
+
+      <div class="max-w-xs">
+        
+
+        <div class="flex items-center gap-2">
+          <img src="/logo.png" class="" />
+          <span class="font-semibold text-[#42389E] text-sm"></span>
+        </div>
+
+
+        <p class="text-sm text-[#868585] mt-4 leading-relaxed">
+          AI-native documentation infrastructure for engineering teams that demand
+          quality, speed, and measurable impact.
+        </p>
+
+
+        <div class="flex gap-4 mt-5">
+          <img src="/linkedin.png" class="w-5 h-5" />
+          <img src="/facebook.png" class="w-5 h-5" />
+          <img src="/insta.png" class="w-5 h-5" />
+        </div>
+      </div>
+
+
+      <div class="grid grid-cols-2 md:grid-cols-3 gap-16 w-full md:w-auto">
+
+
+        <div>
+          <h4 class="font-semibold text-sm text-[#1f1f1f] mb-4">Company</h4>
+          <ul class="space-y-2 text-sm text-[#868585]">
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Blog</li>
+            <li>Partnerships</li>
+          </ul>
+        </div>
+
+
+        <div>
+          <h4 class="font-semibold text-sm text-[#1f1f1f] mb-4">Resources</h4>
+          <ul class="space-y-2 text-sm text-[#868585]">
+            <li>Help Center</li>
+            <li>Fees & Limits</li>
+            <li>System Status</li>
+          </ul>
+        </div>
+
+
+        <div>
+          <h4 class="font-semibold text-sm text-[#1f1f1f] mb-4">
+            Legal & Compliance
+          </h4>
+          <ul class="space-y-2 text-sm text-[#868585]">
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+            <li>Risk Disclosure</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+
+  </footer>
+</template>
