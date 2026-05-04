93dbfcb95e0b108d2cbb2fabc942802ed4c16d2f
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:12:35 +0530

    feature_item_page

---
+++ app/components/ui/FeatureItem.vue
@@ -0,0 +1,32 @@
+<!-- components/ui/FeatureItem.vue -->
+<template>
+  <div class="flex gap-4">
+    <!-- ICON -->
+    <div
+      class="w-10 h-10 rounded-lg flex items-center justify-center"
+      :class="iconBg"
+    >
+      <img :src="icon" class="w-5 h-5" />
+    </div>
+
+    <!-- TEXT -->
+    <div>
+      <h3 class="text-lg font text-black">
+        {{ title }}
+      </h3>
+
+      <p class="mt-1 text-sm text-[#868585] max-w-md">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  icon: String,
+  iconBg: String,
+});
+</script>
