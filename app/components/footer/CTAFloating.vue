dd54728fed81bac5a66696cbf4d2dd3002d6fe20
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:09:12 +0530

    floating_image_btw_two

---
+++ app/components/footer/CTAFloating.vue
@@ -0,0 +1,9 @@
+<!-- components/sections/Footer/CTAFloating.vue -->
+<template>
+  <div class="relative z-10 flex justify-center -mb-32">
+    <img
+      src="/images/footer.png"
+      class="w-full max-w-5xl rounded-2xl shadow-xl"
+    />
+  </div>
+</template>
