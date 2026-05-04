80d3bc3bd153a92e84d691429e82578130fb486c
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:05:59 +0530

    step_card

---
+++ app/components/ui/StepCard.vue
@@ -0,0 +1,79 @@
+<!-- components/ui/StepCard.vue -->
+<template>
+  <div
+    class="flex items-center justify-between p-4 rounded-lg border border-gray-200 bg-[#f5f4f4]"
+  >
+    
+
+    <div class="flex items-center gap-3">
+      
+
+      <div
+        v-if="variant !== 'success'"
+        class="w-8 h-8 rounded-full flex items-center justify-center text-sm font-semibold"
+        :class="numberBg"
+      >
+        {{ step }}
+      </div>
+
+
+      <div
+        v-else
+        class="w-10 h-10 rounded-full flex items-center justify-center bg-green-500 text-white"
+      >
+        <img src="/tick_fourth.png" alt="">
+      </div>
+
+
+      <div>
+        <p class="font-medium text-gray-800">
+          {{ title }}
+        </p>
+
+        <p class="text-sm text-[#868585]">
+          {{ subtitle }}
+        </p>
+      </div>
+    </div>
+
+    <!-- RIGHT SIDE -->
+    <div>
+      
+      <!-- NORMAL STATES -->
+      <span
+        v-if="variant !== 'success' && status === 'done'"
+        class="text-green-500"
+      >
+        ✔
+      </span>
+
+      <span
+        v-else-if="variant !== 'success' && status === 'review'"
+        class="text-orange-500 text-xs font-medium bg-orange-100 px-2 py-1 rounded"
+      >
+        Review
+      </span>
+
+
+      <span
+        v-if="variant === 'success'"
+        class="text-green-500 text-xl"
+      >
+        ☆
+      </span>
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  step: Number,
+  title: String,
+  subtitle: String,
+  status: String,      
+  numberBg: String,    
+  variant: String      
+})
+</script>
---
+++ app/components/ui/UseCaseCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="rounded-xl p-6 border border-gray-200  flex gap-4"
+    :class="bgClass"
+  >
+
+    <div
+      class="w-12 h-12 flex items-center justify-center rounded-lg"
+      :class="iconBg"
+    >
+      <img :src="icon" class="w-6 h-6" />
+    </div>
+
+
+    <div>
+      <p class="text-xs font-medium text-gray-500 uppercase"
+      :class="bgClass ? 'text-white' : 'text-[#42389E]'">
+        {{ label }}
+      </p>
+
+      <h3
+        class="text-lg font-normal mt-1 text-[#42389E]"
+        :class="bgClass ? 'text-white' : 'text-black'"
+      >
+        {{ title }}
+      </h3>
+
+      <p
+        class="mt-2 text-sm text-[#868585] max-w-md"
+        :class="bgClass ? 'text-white/80' : 'text-[#868585]'"
+      >
+        {{ description }}
+      </p>
+
+
+      <ul
+        class="mt-4 space-y-2 text-sm text-[#5d5c5c]"
+        :class="bgClass ? 'text-white/90' : 'text-[#5d5c5c]'"
+      >
+        <li
+          v-for="(item, i) in points"
+          :key="i"
+          class="flex items-center gap-2"
+        >
+          <span class="text-green-500">✔</span>
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  title: String,
+  description: String,
+  points: Array,
+  icon: String,
+  iconBg: String,
+  bgClass: String,
+});
+</script>
