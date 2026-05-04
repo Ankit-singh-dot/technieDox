2f9744cc320ae396ce0bfcfe5e57992860c90315
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:04:14 +0530

    about_me_page

---
+++ app/pages/about.vue
@@ -0,0 +1,11 @@
+<script setup>
+import OverviewSection from "~/components/sections/About/OverviewSection.vue";
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<template>
+  <div>
+    <navbar />
+    <OverviewSection />
+  </div>
+</template>
