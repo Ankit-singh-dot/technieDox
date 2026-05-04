42dbf545f1dc513c8c874a46785c24a1ba090363
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:12:00 +0530

    hero_section

---
+++ app/components/Hero/HeroSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="min-h-screen flex items-center bg-gray-50">
+    <div class="container mx-auto px-6 grid md:grid-cols-2 gap-10">
+      <HeroText />
+      <HeroImage />
+    </div>
+  </section>
+</template>
+
+<script setup>
+// import HeroText from "./HeroText.vue";
+import HeroImage from "./HeroImage.vue";
+</script>
