d54b5b649c46965657424aed7c1b5d8193b3f7b7
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:03:25 +0530

    pricing_section

---
+++ app/pages/pricing.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Pricing from "~/components/sections/PricingPage/ Pricing.vue";
+import ComparisonSection from "~/components/sections/Comparison/ComparisonSection.vue";
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <Pricing />
+    <ComparisonSection />
+  </div>
+</template>
