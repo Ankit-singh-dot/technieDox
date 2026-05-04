0ce96634507e8ca2768b6974017a9e795a5f3bf9
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:14:02 +0530

    analytics_section

---
+++ app/components/sections/AnalyticsSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="py-20">
+    <!-- HEADER -->
+    <div class="max-w-6xl mx-auto text-center px-4">
+      <p class="text-orange-500 text-sm uppercase">ROI & Analytics</p>
+
+      <h2 class="text-3xl md:text-4xl font-bold text-purple-700 mt-2">
+        Enterprise-Grade ROI And Analytics
+      </h2>
+
+      <p class="mt-4 text-gray-600 max-w-2xl mx-auto">
+        The first documentation platform with measurable business impact. Track
+        time saved, productivity gains, and documentation health in real-time.
+      </p>
+
+      <div class="mt-10">
+        <img src="/executive_roi_page.png" alt="" />
+      </div>
+    </div>
+  </section>
+</template>
