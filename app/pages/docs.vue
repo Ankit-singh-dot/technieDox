f857a1c2dd5946121f746c8e7323783a6defe4f4
Ankit <ankitrajdihara123@gmail.com>
2026-05-04 14:03:46 +0530

    docs_main

---
+++ app/pages/docs.vue
@@ -0,0 +1,13 @@
+<script setup>
+import DocsSection from "~/components/sections/Docs/DocsSection.vue";
+import Navbar from "~/components/Navbar.vue";
+import QuickStart from "~/components/sections/Docs/QuickStart.vue";
+import AIFeatures from "~/components/sections/Docs/AIFeatures.vue";
+</script>
+
+<template>
+  <Navbar />
+  <DocsSection />
+  <QuickStart />
+  <AIFeatures />
+</template>
